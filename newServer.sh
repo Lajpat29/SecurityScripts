9ad076358283b301d60cba32907e218d56d6d20c
Lajpat <lajpat29@gmail.com>
2015-12-29 20:48:08 +0530

    Done some more changes.

--- newServer.sh
+++ newServer.sh
@@ -4,6 +4,7 @@ cp /etc/passwd /root/passwd.bak
 cp /etc/ssh/sshd_config /root/sshd_config.bak
 sshlogin_home_dir=/home/sshlogin
 echo_command=echo
+apt-get update && apt-get install sudo -y
 
 cp /etc/sudoers /root/sudoers.bak
 #Create new user with name sshlogin with Locked account
